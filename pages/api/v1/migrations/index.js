3d4a268798ed038988f1a847660697bc956bffd7
Lucas Nunes <lucasnp.dev@pm.me>
2026-01-17 10:39:46 -0300

    fix bug `not end connection db`  when request migrations with not allowed methods

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,37 +3,49 @@ import { join } from "node:path";
 import database from "infra/database";
 
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
+  const allowedMethods = ["GET", "POST"];
 
-  const defaultMigrationOptions = {
-    dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
-
-  if (request.method === "GET") {
-    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-    await dbClient.end();
-    response.status(200).json(pendingMigrations);
+  if (!allowedMethods.includes(response.method)) {
+    return response.status(405).end();
   }
 
-  if (request.method === "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dryRun: false,
-    });
+  let dbClient;
 
-    await dbClient.end();
+  try {
+    dbClient = await database.getNewClient();
 
-    if (migratedMigrations.length > 0) {
-      response.status(201).json(migratedMigrations);
+    const defaultMigrationOptions = {
+      dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
+
+    if (request.method === "GET") {
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+      await dbClient.end();
+      response.status(200).json(pendingMigrations);
     }
 
-    response.status(200).json(migratedMigrations);
-  }
+    if (request.method === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationOptions,
+        dryRun: false,
+      });
 
-  return response.status(405).end();
+      await dbClient.end();
+
+      if (migratedMigrations.length > 0) {
+        response.status(201).json(migratedMigrations);
+      }
+
+      response.status(200).json(migratedMigrations);
+    }
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await dbClient.end();
+  }
 }
---
+++ tests/integration/api/v1/migrations/put.test.js
@@ -0,0 +1,6 @@
+test("PUT to /api/v1/migrations should return 405", async () => {
+  const putMigrations = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "PUT",
+  });
+  expect(putMigrations.status).toBe(405);
+});
